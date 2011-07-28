e26122ec5bdd8aad27b098771183c38b540c7cf9
Jorge Zapata <jorgeluis.zapata@gmail.com>
2011-07-28 00:40:11 +0000

    Port the image example too

--- src/tests/eon_test_image.c
+++ src/tests/eon_test_image.c
@@ -21,13 +21,15 @@ int main(int argc, char **argv)
 	eon_init();
 	ecore_init();
 
+	backend = eon_ecore_sdl_new();
+	//backend = eon_ecore_remote_new();
+
 	layout = eon_stack_new();
 	eon_element_width_set(layout, 320.0);
 	eon_element_height_set(layout, 240.0);
 
-	eon_stack_direction_set(layout, EON_STACK_DIRECTION_VERTICAL);
-	backend = eon_ecore_sdl_new();
 	win = eon_window_new(backend, layout, 320, 240);
+	eon_stack_direction_set(layout, EON_STACK_DIRECTION_VERTICAL);
 	image = eon_image_new();
 	eon_image_file_set(image, argv[1]);
 	eon_layout_child_add(layout, image);
