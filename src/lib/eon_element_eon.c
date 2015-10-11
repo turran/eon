d406261da14981ee272b21ba44e0094afb4cdce8
Jorge Zapata <jorgeluis.zapata@gmail.com>
2015-10-11 21:14:26 +0200

    Remove obsolete code
    We now get the size from what the window feature has. It is
    the size the user provides

--- src/lib/eon_element_eon.c
+++ src/lib/eon_element_eon.c
@@ -70,105 +70,6 @@ static void _eon_element_eon_timeline_cb(Egueb_Dom_Event *e,
 	printf("requesting a timeline!!!\n");
 }
 
-#if 0
-static void _eon_element_eon_request_geometry_cb(Egueb_Dom_Event *ev, void *data)
-{
-	Eon_Document *thiz;
-	Eon_Size size;
-	Eon_Renderable_Size renderable_size;
-	Egueb_Dom_Node *n = data;
-	Egueb_Dom_Node *eon;
-	Eina_Rectangle geometry;
-	int size_hints;
-
-	/* FIXME this event is not used on any application, we can safely
-	 * remove it. For now, whenever the topmost element requests a geometry
-	 * we return the current width/height set through the window feature
-	 */
-	thiz = egueb_dom_document_external_data_get(n);
-	eina_rectangle_coords_from(&geometry, 0, 0, thiz->width, thiz->height);
-	eon_event_geometry_request_geometry_set(ev, &geometry);
-	return;
-
-	DBG("Rquesting main geometry");
-	eon = egueb_dom_document_document_element_get(n);
-	if (!eon)
-	{
-		ERR("No topmost element found");
-		return;
-	}
-
-	size_hints = eon_renderable_size_hints_get(eon, &renderable_size);
-	DBG("Main hints are %08x", size_hints);
-	if (size_hints & EON_RENDERABLE_HINT_MIN_MAX)
-	{
-		/* TODO set the min size for now */
-		size.width = renderable_size.min_width;
-		size.height = renderable_size.min_height;
-	}
-	else
-	{
-		size.width = 0;
-		size.height = 0;
-	}
-	eina_rectangle_coords_from(&geometry, 0, 0, size.width, size.height);
-	eon_event_geometry_request_geometry_set(ev, &geometry);
-	egueb_dom_node_unref(eon);
-
-	/* Se the width so the user when requesting the geometry will get this value */
-	thiz = egueb_dom_document_external_data_get(n);
-	thiz->width = size.width;
-	thiz->height = size.height;
-}
-#endif
-
-static void _eon_element_eon_node_inserted_cb(Egueb_Dom_Event *ev,
-		void *data)
-{
-	Egueb_Dom_Node *n = data;
-	Egueb_Dom_Node *parent;
-	Egueb_Dom_Node *target;
-
-	parent = egueb_dom_event_mutation_related_get(ev);
-
-	if (parent != n)
-	{
-		egueb_dom_node_unref(parent);
-		return;
-	}
-
-	egueb_dom_node_unref(parent);
-	target = EGUEB_DOM_NODE(egueb_dom_event_target_get(ev));
-#if 0
-	egueb_dom_event_target_event_listener_add(target, EON_EVENT_GEOMETRY_REQUEST,
-			_eon_element_eon_request_geometry_cb, EINA_FALSE, n);
-#endif
-	egueb_dom_node_unref(target);
-}
-
-static void _eon_element_eon_node_removed_cb(Egueb_Dom_Event *ev,
-		void *data)
-{
-	Egueb_Dom_Node *n = data;
-	Egueb_Dom_Node *parent;
-	Egueb_Dom_Node *target;
-
-	parent = egueb_dom_event_mutation_related_get(ev);
-	if (parent != n)
-	{
-		egueb_dom_node_unref(parent);
-		return;
-	}
-
-	egueb_dom_node_unref(parent);
-	target = EGUEB_DOM_NODE(egueb_dom_event_target_get(ev));
-#if 0
-	egueb_dom_event_target_event_listener_add(target, EON_EVENT_GEOMETRY_REQUEST,
-			_eon_element_eon_request_geometry_cb, EINA_FALSE, n);
-#endif
-	egueb_dom_node_unref(target);
-}
-
 /* Whenever an invalidate geometry event reaches the topmost element,
  * just enqueue the element for later processing
  */
@@ -446,14 +347,6 @@ static void _eon_element_eon_init(Eon_Widget *w)
 			EON_EVENT_GEOMETRY_INVALIDATE,
 			_eon_element_eon_geometry_invalidate_cb, EINA_FALSE,
 			w);
-	egueb_dom_event_target_event_listener_add(n,
-			EGUEB_DOM_EVENT_MUTATION_NODE_INSERTED,
-			_eon_element_eon_node_inserted_cb,
-			EINA_FALSE, n);
-	egueb_dom_event_target_event_listener_add(n,
-			EGUEB_DOM_EVENT_MUTATION_NODE_REMOVED,
-			_eon_element_eon_node_removed_cb,
-			EINA_FALSE, n);
 
 	thiz = EON_ELEMENT_EON(w);
 	thiz->input = egueb_dom_input_new(&_eon_element_eon_input_descriptor, n);
@@ -477,25 +370,14 @@ static Eina_Bool _eon_element_eon_pre_process(Eon_Widget *w)
 	/* set the size based on the user provided size */
 	if (r->needs_geometry)
 	{
-		Egueb_Dom_Event *ev;
-		Eina_Rectangle geom;
-		Eina_Bool set;
+		Eon_Element_Eon *thiz;
 		Egueb_Dom_Node *n;
+		Eina_Rectangle geom;
+
+		thiz = EON_ELEMENT_EON(w);
 
 		n = (EON_ELEMENT(r))->n;
-		/* request a geometry upstream */
-		ev = eon_event_geometry_request_new();
-		egueb_dom_event_target_event_dispatch(n, egueb_dom_event_ref(ev), NULL, NULL);
-		/* check that we have a valid geometry */
-		set = eon_event_geometry_request_geometry_get(ev, &geom);
-		egueb_dom_event_unref(ev);
-
-		if (!set)
-		{
-			ERR("Application does not set a geometry");
-			return EINA_FALSE;
-
-		}
+		eina_rectangle_coords_from(&geom, 0, 0, thiz->width, thiz->height);
 		eon_renderable_geometry_set(n, &geom);
 	}
 	return EINA_TRUE;
