652b7ec9151c3063ebb85385ccc914b088c85873
Jorge Zapata <jorgeluis.zapata@gmail.com>
2015-10-26 00:14:47 +0100

    Start adding the future label theme element

---
+++ src/lib/theme/eon_theme_element_label.c
@@ -0,0 +1,163 @@
+/* EON - Canvas and Toolkit library
+ * Copyright (C) 2008-2011 Jorge Luis Zapata
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+#include "eon_theme_private.h"
+#include "eon_theme_element_label.h"
+#include "eon_theme_main_private.h"
+#include "eon_theme_renderable_private.h"
+/*============================================================================*
+ *                                  Local                                     *
+ *============================================================================*/
+#define EON_THEME_ELEMENT_LABEL_DESCRIPTOR eon_theme_element_label_descriptor_get()
+#define EON_THEME_ELEMENT_LABEL(o) ENESIM_OBJECT_INSTANCE_CHECK(o,			\
+		Eon_Theme_Element_Label, EON_THEME_ELEMENT_LABEL_DESCRIPTOR)
+
+typedef struct _Eon_Theme_Element_Label
+{
+	Eon_Theme_Renderable base;
+	const Eon_Theme_Element_Label_Descriptor *d;
+	void *data;
+} Eon_Theme_Element_Label;
+
+typedef struct _Eon_Theme_Element_Label_Class
+{
+	Eon_Theme_Renderable_Class base;
+} Eon_Theme_Element_Label_Class;
+
+/*----------------------------------------------------------------------------*
+ *                           Renderable interface                             *
+ *----------------------------------------------------------------------------*/
+static Enesim_Renderer * _eon_theme_element_label_renderer_get(Eon_Theme_Renderable *r)
+{
+	Eon_Theme_Element_Label *thiz;
+
+	thiz = EON_THEME_ELEMENT_LABEL(r);
+	if (thiz->d->renderer_get)
+		return thiz->d->renderer_get(thiz->data);
+	return NULL;
+}
+/*----------------------------------------------------------------------------*
+ *                             Element interface                              *
+ *----------------------------------------------------------------------------*/
+static Egueb_Dom_Node * _eon_theme_element_label_ctor(Eon_Theme_Element *e)
+{
+	Eon_Theme_Element_Label *thiz;
+
+	thiz = EON_THEME_ELEMENT_LABEL(e);
+	if (thiz->d->ctor)
+		return thiz->d->ctor();
+	return NULL;
+}
+
+static Eina_Bool _eon_theme_element_label_process(Eon_Theme_Element *e)
+{
+	Eon_Theme_Element_Label *thiz;
+
+	thiz = EON_THEME_ELEMENT_LABEL(e);
+	if (thiz->d->process)
+		return thiz->d->process(thiz->data);
+	return EINA_TRUE;
+}
+
+static Egueb_Dom_String * _eon_theme_element_label_tag_name_get(Eon_Theme_Element *e)
+{
+	Eon_Theme_Element_Label *thiz;
+
+	thiz = EON_THEME_ELEMENT_LABEL(e);
+	if (thiz->d->tag_name_get)
+	{
+		const char *name;
+		name = thiz->d->tag_name_get();
+		return egueb_dom_string_new_with_static_string(name);
+	}
+	return NULL;
+}
+/*----------------------------------------------------------------------------*
+ *                              Object interface                              *
+ *----------------------------------------------------------------------------*/
+ENESIM_OBJECT_INSTANCE_BOILERPLATE(EON_THEME_RENDERABLE_DESCRIPTOR,
+		Eon_Theme_Element_Label, Eon_Theme_Element_Label_Class, eon_theme_element_label);
+
+static void _eon_theme_element_label_class_init(void *k)
+{
+	Eon_Theme_Renderable_Class *klass;
+	Eon_Theme_Element_Class *e_klass;
+
+	klass = EON_THEME_RENDERABLE_CLASS(k);
+	klass->renderer_get = _eon_theme_element_label_renderer_get;
+
+	e_klass = EON_THEME_ELEMENT_CLASS(k);
+	e_klass->ctor = _eon_theme_element_label_ctor;
+	e_klass->tag_name_get = _eon_theme_element_label_tag_name_get;
+	e_klass->process = _eon_theme_element_label_process;
+}
+
+static void _eon_theme_element_label_instance_init(void *o)
+{
+}
+
+static void _eon_theme_element_label_instance_deinit(void *o)
+{
+	Eon_Theme_Element_Label *thiz;
+
+	thiz = EON_THEME_ELEMENT_LABEL(o);
+	if (thiz->d->dtor)
+		thiz->d->dtor(thiz->data);
+}
+/*============================================================================*
+ *                                 Global                                     *
+ *============================================================================*/
+/*============================================================================*
+ *                                   API                                      *
+ *============================================================================*/
+/**
+ * Creates a new label theme
+ * @param d The set of functions the theme implemenation must implement
+ * @param data The private data passed in on the descriptor functions 
+ */
+EAPI Egueb_Dom_Node * eon_theme_element_label_new(
+		const Eon_Theme_Element_Label_Descriptor *d, void *data)
+{
+	Eon_Theme_Element_Label *thiz;
+	Eon_Theme_Element *e;
+	Egueb_Dom_Node *n;
+
+	n = EON_THEME_ELEMENT_NEW(eon_theme_element_label);
+	thiz = egueb_dom_element_external_data_get(n);
+	thiz->d = d;
+	thiz->data = data;
+
+	e = EON_THEME_ELEMENT(thiz);
+	return e->n;
+}
+
+/**
+ * Gets the private data of a label
+ * @param w The label to get the data from
+ * @return The private data
+ */
+EAPI void * eon_theme_element_label_data_get(Egueb_Dom_Node *n)
+{
+	Eon_Theme_Element_Label *thiz;
+	Eon_Theme_Element *e;
+
+	e = EON_THEME_ELEMENT(egueb_dom_element_external_data_get(n));
+	thiz = EON_THEME_ELEMENT_LABEL(e);
+
+	return thiz->data;
+}
+
