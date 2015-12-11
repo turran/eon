4a846220060e40290328eb2155608d1ae0e55794
Jorge Zapata <jorgeluis.zapata@gmail.com>
2015-12-11 18:33:17 +0100

    Add the value implementation

---
+++ src/lib/value/Makefile.mk
@@ -0,0 +1,3 @@
+src_lib_libeon_la_SOURCES += \
+src/lib/value/eon_value_theme.c \
+src/lib/value/eon_value_theme_private.h
---
+++ src/lib/value/eon_value_theme.c
@@ -0,0 +1,39 @@
+/* EON - Canvas and Toolkit library
+ * Copyright (C) 2008-2015 Jorge Luis Zapata
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+#include "eon_private.h"
+#include "eon_theme.h"
+
+#include "eon_theme_private.h"
+/*============================================================================*
+ *                                  Local                                     *
+ *============================================================================*/
+/*----------------------------------------------------------------------------*
+ *                             Value interface                                *
+ *----------------------------------------------------------------------------*/
+EGUEB_DOM_VALUE_PRIMITIVE_BOILERPLATE(eon_theme, Eon_Theme);
+/*============================================================================*
+ *                                 Global                                     *
+ *============================================================================*/
+const Egueb_Dom_Value_Descriptor * eon_value_theme_descriptor_get(void)
+{
+	return &_eon_theme_descriptor;
+}
+/*============================================================================*
+ *                                   API                                      *
+ *============================================================================*/
+
