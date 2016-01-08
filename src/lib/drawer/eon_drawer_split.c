51c20d892e48692391c35367aa82d50dc3e5121f
Jorge Zapata <jorgeluis.zapata@gmail.com>
2016-01-08 01:42:13 +0100

    Remove obsolete files

--- src/lib/drawer/eon_drawer_split.c
+++
@@ -1,4 +0,0 @@
-/* A split must support any height/width based on the orientation
- * The drawer implementation should get the thickness of split
- * The drawer implementation should set the geometry
- */
